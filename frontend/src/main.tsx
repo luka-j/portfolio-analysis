996e0ed3d92ae35356821dde9ad4c674fc36ce5f
Luka Jovicic <11800546+luka-j@users.noreply.github.com>
2026-05-24 20:20:18 +0200

    Improve loading experience

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,10 @@ import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister'
 import App from './App'
 import ErrorBoundary from './components/ErrorBoundary'
 import { PrivacyProvider } from './utils/PrivacyContext'
@@ -11,22 +13,30 @@ import { PrivacyProvider } from './utils/PrivacyContext'
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      gcTime: 1000 * 60 * 60 * 24 * 7, // 7 days: keep cached data around for 7 days
       staleTime: 5 * 60 * 1000, // 5 minutes
     },
   },
 })
 
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
-      <QueryClientProvider client={queryClient}>
+      <PersistQueryClientProvider
+        client={queryClient}
+        persistOptions={{ persister }}
+      >
         <BrowserRouter>
           <PrivacyProvider>
             <App />
           </PrivacyProvider>
         </BrowserRouter>
         <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+      </PersistQueryClientProvider>
     </ErrorBoundary>
   </StrictMode>,
 )
